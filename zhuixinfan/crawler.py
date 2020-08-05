116955628c240e0c87f18250ababe7b37050f672
will <william.sv@icloud.com>
2020-08-06 00:35:00 +0800

    add zhuixinfan crawler

---
+++ zhuixinfan/crawler.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+ @Project   : movie_crawler
+ @FileName  : crawler.py
+ @Time      : 2020/8/5 11:37 下午
+ @Author    : william.sv@icloud.com
+
+"""
